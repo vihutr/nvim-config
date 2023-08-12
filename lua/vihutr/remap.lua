1d80d585ba1bdf4c8994ac1dc865720419174c3a
vihutr <vihutr@gmail.com>
2023-08-12 15:54:14 -0700

    changed :Ex keymap from pv to fv

--- lua/vihutr/remap.lua
+++ lua/vihutr/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 -- paste over something highlighted and not copy to buffer
 vim.keymap.set("x", "<leader>p", "\"_dP")
