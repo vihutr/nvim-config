ad4f32d5b6b9237c088aff7ce9c91358f41ee008
vihutr <vihutr@gmail.com>
2023-08-11 23:34:25 -0700

    updated remaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,20 +15,21 @@ lsp.ensure_installed({
 local cmp = require('cmp')
 local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
+ 
 	["C-Space>"] = cmp.mapping.complete(),
-})
-
-lsp.set_preferences({
-  sign_icons = { }
 })
 
 lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
 })
 
+lsp.set_preferences({
+  sign_icons = { }
+})
+
+-- remaps only for current buffer; if there is an lsp use its buffer, otherwise do vim's default
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
--- lua/vihutr/remap.lua
+++ lua/vihutr/remap.lua
@@ -1,2 +1,17 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- paste over something highlighted and not copy to buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to system clipboard instead of just vim
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("x", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y",  "\"+Y")
+
+-- delete to void instead of copy to buffer
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
+
