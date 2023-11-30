79be92c3a1b4613e8772e470a86737032a577717
vihutr <vihutr@gmail.com>
2023-11-29 16:56:39 -0800

    comment

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,10 @@
-
 function CustomColor(color)
-	color = color or "rose-pine"
+    vim.opt.termguicolors = true
+	color = colors or "rose-pine"
 	vim.cmd.colorscheme(color)
-	
-	vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+	-- set transparent bg
+    vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
-	
 end
 
 CustomColor()
