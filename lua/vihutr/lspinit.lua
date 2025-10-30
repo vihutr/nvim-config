020a93f741c0364e9f025e06db8a9e1c15149c08
vihutr <vihutr@gmail.com>
2025-10-30 12:08:29 -0700

    lsp

--- lua/vihutr/lspinit.lua
+++ lua/vihutr/lspinit.lua
@@ -1,39 +1,45 @@
 require('mason').setup({})
--- require('mason-lspconfig').setup({
---   handlers = {
---     function(server_name)
---       require('lspconfig')[server_name].setup({})
---     end,
---   },
--- })
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
 
--- recognize vim as global
-require'lspconfig'.lua_ls.setup {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {'vim'}
-            }
-        }
-    }
-}
-require'lspconfig'.pylsp.setup{
-    settings = {
-        pylsp = {
-            plugins = {
-                pycodestyle = {
-                    ignore = {'W391', 'E501', 'E401', 'E231', 'E302', 'E303', 'E305', 'W292', 'W293'}
-                },
-                -- pylint = { enabled = true },
-                jedi_completion = { fuzzy = true },
-                yapf = { enabled = true },
-                pylsp_mypy = { enabled = true },
-                pyls_isort = { enabled = true },
-                black = { enabled = true },
-            }
-        }
-    }
-}
+-- require'lspconfig'.eslint.setup({
+--     settings = {
+--     },
+-- })
+-- 
+-- -- recognize vim as global
+-- require'lspconfig'.lua_ls.setup({
+--     settings = {
+--         Lua = {
+--             diagnostics = {
+--                 globals = {'vim'}
+--             }
+--         }
+--     }
+-- })
+-- 
+-- require'lspconfig'.pylsp.setup({
+--     settings = {
+--         pylsp = {
+--             plugins = {
+--                 pycodestyle = {
+--                     ignore = {'W391', 'E501', 'E401', 'E231', 'E302', 'E303', 'E305', 'W292', 'W293'}
+--                 },
+--                 -- pylint = { enabled = true },
+--                 jedi_completion = { fuzzy = true },
+--                 yapf = { enabled = true },
+--                 pylsp_mypy = { enabled = true },
+--                 pyls_isort = { enabled = true },
+--                 black = { enabled = true },
+--             }
+--         }
+--     }
+-- })
 
 local cmp = require('cmp')
 cmp.setup({
